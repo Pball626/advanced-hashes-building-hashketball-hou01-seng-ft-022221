35aee6126cdb971ff1c20a8d597a2a4e7136a344
Jose Mendoza <pball626@gmail.com>
2021-02-12 20:55:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,6 @@
 def game_hash
   hasketball_hash = {:home => {:team_name => "Brooklyn Nets", 
-  :colors => "", 
+  :colors => "Black White", 
   :players => 
   :player_name => "Alan Anderson",
   :number => 0,
@@ -50,9 +50,9 @@ def game_hash
   :assists => 2,
   :steals => 4,
   :blocks => 11,
-  :slam_dunks => 1}
+  :slam_dunks => 1},
   
-  :away => {:team_name => "Charolette Hornets",
+  {:away => {:team_name => "Charolette Hornets",
   :colors => "Turqoise, Purple",
   :players => 
   :player_name => }}
