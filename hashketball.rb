064e49a6ea39b06b21457d9044affd825f275ddd
Jose Mendoza <pball626@gmail.com>
2021-02-12 21:47:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -109,6 +109,18 @@ def game_hash
           :steals => 4,
           :blocks => 5,
           :slam_dunks => 5,
+        },
+        {
+          :player_name => 
+          :number => 
+          :shoe =>
+          :points =>
+          :rebounds =>
+          :assists =>
+          :steals =>
+          :blocks =>
+          :slam_dunks =>
+        }
       ]
     
   }
